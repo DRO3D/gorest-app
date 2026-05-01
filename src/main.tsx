fd2ebd8aaf87c02a44e70fe4fab37de54ef9a0ef
MAIN-DRO3D <misha.stepanyk2003@gmail.com>
2026-05-01 13:26:08 +0300

    fix for github pages

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import { BrowserRouter } from 'react-router-dom';
+import { HashRouter } from 'react-router-dom';
 import { Theme, presetGpnDefault } from '@consta/uikit/Theme';
 import { App } from './App';
 import { AuthProvider } from './hooks/useAuth';
@@ -10,9 +10,9 @@ ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
     <Theme preset={presetGpnDefault}>
       <AuthProvider>
-        <BrowserRouter>
+        <HashRouter>
           <App />
-        </BrowserRouter>
+        </HashRouter>
       </AuthProvider>
     </Theme>
   </React.StrictMode>
